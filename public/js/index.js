78ff246314d1c3563160d186ddbfc1b3ae46d432
Vamseekrishna Kattika <vamseekrishna.kattika@gmail.com>
2019-02-13 21:03:57 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('createMessage',{
-    from:'jen@example.com',
-    text:'Hey John'
-  });
 });
 
 socket.on('disconnect', function () {
